8352b2c1a672a6ad6aaf3b2f8bb63654366c1798
NikitOs <exemplum777@gmail.com>
2020-12-20 18:45:20 +0300

    Подчистил за Лёхой

--- int.js
+++ int.js
@@ -1,12 +1,11 @@
 let menuLin = document.getElementById('menuLin')
 let menuExp = document.getElementById('menuExp')
-let show = document.getElementById('show')
-let hide = document.getElementById('hide')
+let show = document.getElementById('show-button')
+let hide = document.getElementById('hide-button')
 let help = document.getElementById('help')
 let helpMenu = document.getElementById('helpMenu')
 
 hide.addEventListener('click', ev => {
-	ev.preventDefault()
 	menuLin.classList.add('hide')
 	menuExp.classList.add('hide')
 	hide.classList.add('hide')
@@ -14,7 +13,6 @@ hide.addEventListener('click', ev => {
 })
 
 show.addEventListener('click', ev => {
-	ev.preventDefault()
 	menuLin.classList.remove('hide')
 	menuExp.classList.remove('hide')
 	hide.classList.remove('hide')
